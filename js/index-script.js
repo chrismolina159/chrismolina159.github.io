63436ab2c6e4666f14bf81e8f91db9d3c7c0aed7
Chris M <christian.molina159@gmail.com>
2019-07-15 03:18:50 -0400

    Test 8 media query iphone 8

--- js/index-script.js
+++ js/index-script.js
@@ -1,5 +1,5 @@
 var i = 0;
-var str1 = "Welcome to my website";
+var str1 = "Home Page";				//9
 var str2 = "Resume";				//6
 var str3 = "Classes Taken"; 		//13
 var str4 = "Github";				//6
@@ -12,23 +12,23 @@ var headerSpeed = 400;
 //add media for tablet and phone
 function typeWrite(){
 
-	if(i < str1.length){
-		document.getElementById("typewriter").innerHTML += str1.charAt(i);
+	if(i < str3.length){
+		document.getElementById("cla").innerHTML += str3.charAt(i);
 		if(i < 6){
+			document.getElementById("typewriter").innerHTML += str1.charAt(i);
 			document.getElementById("res").innerHTML += str2.charAt(i);
-			document.getElementById("cla").innerHTML += str3.charAt(i);
 			document.getElementById("git").innerHTML += str4.charAt(i);
 			document.getElementById("lin").innerHTML += str5.charAt(i);
 			document.getElementById("abt").innerHTML += str6.charAt(i);
 			document.getElementById("quo").innerHTML += str7.charAt(i);
 		}
 		else if(i < 8){
-			document.getElementById("cla").innerHTML += str3.charAt(i);
+			document.getElementById("typewriter").innerHTML += str1.charAt(i);
 			document.getElementById("lin").innerHTML += str5.charAt(i);
 			document.getElementById("abt").innerHTML += str6.charAt(i);
 		}
-		else if(i < 13){
-			document.getElementById("cla").innerHTML += str3.charAt(i);
+		else if(i < 9){
+			document.getElementById("typewriter").innerHTML += str1.charAt(i);
 		}
 		i++;
 		setTimeout(typeWrite, headerSpeed);
